49f2d37529c7e0c4a672c233b92609ccaa7f0fc9
Jesse Calton <jessecalton@gmail.com>
2020-03-15 16:14:44 -0700

    adds hovering tooltip

--- src/components/Arizmendi.js
+++ src/components/Arizmendi.js
@@ -1,25 +1,37 @@
 import React, {Component} from 'react';
+import ReactTooltip from 'react-tooltip'
 const axios = require('axios');
 // Need this to bypass the CORS denial in localhost...
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 class Arizmendi extends Component {
     state = {
-        todaysPizza: ''
+        todaysPizza: 'Loading...'
     }
-    
+    // Takes the HTML pizza string and makes it look like a real menu
+    sanitizePizzaMenu = (pizza) => {
+        // Remove the HTML
+        let pizzaString = pizza.split('class="active"')[1].split('</td>')[0].split('"yasp-item">')[1].split('</p>')[0];
+        let formattedPizza = pizzaString.replace('p&p', 'parmesan & parsley')
+        // Titleize the pizza toppings
+        formattedPizza = formattedPizza.split(' ').map(ingredient => ingredient.charAt(0).toUpperCase() + ingredient.substring(1)).join(' ')
+        return formattedPizza;
+    }
     // Updates the state w/ today's Arizmendi pizza once component mounts
     async componentDidMount () {
-            try {
-                const taste = await axios.get(proxyurl + 'https://www.arizmendibakery.com/pizza')
-                let pizzaStuffHTML = await taste.data.split('class="active"')[1].split('</td>')[0];
-                let pizzaStuff = pizzaStuffHTML.split('"yasp-item">')[1].split('</p>')
-                console.log(pizzaStuff)
-                this.setState({todaysPizza: pizzaStuff})
-            } catch (err) {
-                console.log('No Pizza Today...')
-            }
+        try {
+            const taste = await axios.get(proxyurl + 'https://www.arizmendibakery.com/pizza')
+            let pizzaStuffHTML = await taste.data
+            console.log(pizzaStuffHTML)
+            this.setState(() => {
+                const todaysPizza = this.sanitizePizzaMenu(pizzaStuffHTML);
+                return {todaysPizza: todaysPizza}
+            })
+        } catch (err) {
+            console.log('No Pizza Today...')
         }
+    }
+    
     // Gets todays date formatted like 03/15/2020
     getTodaysDate = () => {
     let today = new Date();
@@ -33,8 +45,18 @@ class Arizmendi extends Component {
     render() {
         return (
             <div>
-                <h4>Need ideas? Today, which is {this.getTodaysDate()}, Arizmendi's Pizza is serving:</h4>
-                <p>{this.state.todaysPizza}</p>
+                <h4 className='ideas-header'>Need ideas? Today, which is {this.getTodaysDate()}, <a href="https://www.arizmendibakery.com/pizza">Arizmendi's Pizza</a> is serving:</h4>
+                <div className='ideas'>{this.state.todaysPizza} 
+                    <span aria-label="savouring food" role="img"> 😋</span>
+                </div>
+                <div>
+                    <span className='pointer-emojis' aria-label="pointing right" role="img"> 👉</span>
+                        <span className='tooltip' data-tip="This app doesn't actually order pizza. You should go get it from Arizmendi Bakery.">
+                        Hover over me
+                        </span>
+                    <span className='pointer-emojis' aria-label="pointing left" role="img">👈</span>
+                </div>
+                <ReactTooltip />
             </div>
         )
     }
